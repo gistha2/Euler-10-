269384f3ce8c3912ffae2b83250e624f9b1c1faa
gistha2 <gistha2@gmail.com>
2016-05-28 22:02:36 +0100

    Add files via upload

---
+++ Project Euler 10 - Summation of Primes/Project Euler 10 - Summation of Primes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler_10___Summation_of_Primes
+{
+    class Program
+    {
+
+        public static bool isPrime(int number)
+        {
+            int boundary = Convert.ToInt32(Math.Floor(Math.Sqrt(number)));
+
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            long summation = 0;
+
+            for (int i =1; i<2000000; i++)
+            { if (isPrime(i) == true)
+                {
+                    summation = summation + i;
+                    
+                }
+            }
+            Console.WriteLine(summation);
+            Console.ReadLine();
+
+        }
+    }
+}
